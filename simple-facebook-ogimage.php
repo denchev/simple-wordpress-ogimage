5c25bf3f499f42a55856d75cfc5aa033b24958ba
Marush Denchev <avreon@gmail.com>
2015-01-02 10:30:51 +0200

    Search post content for image to use as an Open Graph one.

--- simple-facebook-ogimage.php
+++ simple-facebook-ogimage.php
@@ -13,9 +13,33 @@
 if( ! function_exists( 'sfogi_wp_head' ) ) {
 	function sfogi_wp_head() {
 		if(is_single() ) {
-			$post_id = get_the_ID();
-			$image = wp_get_attachment_image_src( get_post_thumbnail_id($post_id), 'single-post-thumbnail' );
-			echo '<meta property="og:image" content="' . $image[0] . '">';
+
+			$og_image 	= null;
+			$post_id 	= get_the_ID();
+			$image 		= wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
+
+			// There is a featured image
+			if($image !== false) {
+
+				$og_image = $image[0];
+
+			} else {
+
+				$post = get_post($post_id);
+
+				// Get all images from within post content
+				preg_match_all('/<img(.*?)src="(?P<src>.*?)"([^>]+)>/', $post->post_content, $matches);
+
+				if(isset($matches['src'][0])) {
+					$og_image = $matches['src'][0];
+				}
+
+			}
+
+			if($og_image != null) {
+
+				echo '<meta property="og:image" content="' . $og_image . '">';
+			}
 		}
 	}
 }
